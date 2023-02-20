445afb85b32e9358804773553eb202df58176c3d
chenyl <chenyl@ppwrite.com>
2023-02-20 18:12:12 +0800

    init

---
+++ interfaces/src/main/java/ink/dean/doma/interfaces/InterfacesApplication.java
@@ -0,0 +1,11 @@
+package ink.dean.doma.interfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfacesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InterfacesApplication.class);
+    }
+}
